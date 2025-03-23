bb058d372b3ae90b882245761a91a07df2e8309a
jonase-eldho <eldhojonase@gmail.com>
2025-03-23 15:04:24 +0530

    login nd reg ui

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -15,7 +15,7 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
+      backgroundColor: Colors.white,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
